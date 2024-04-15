b006c13a88419bef9a9b42647ca623b1ba43cf80
kurves <kavssimply@gmail.com>
2024-04-15 10:17:21 -0700

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -20,8 +20,11 @@ class BaseModel:
                                                          '%Y-%m-%dT%H:%M:%S.%f')
             else:
                 kwargs['updated_at'] = datetime.now()
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            if kwargs.get('created_at'):
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                kwargs['created_at'] = datetime.now()
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
