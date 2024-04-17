2bbc547d8869276c4251a2d5b0a2d9d6126aeec2
kurves <kavssimply@gmail.com>
2024-04-17 00:51:06 -0700

    DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
+
+    @property
+    def cities(self):
+        """Getter attribute that returns the list of City instances"""
+        from models import storage
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
