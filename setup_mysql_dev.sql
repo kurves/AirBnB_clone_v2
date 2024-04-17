3415040cb3927004500e9bee4b8ad9661a434832
kurves <kavssimply@gmail.com>
2024-04-17 00:20:27 -0700

    script that prepares a MySQL server for the project:

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,12 @@
+-- create databaseh
+-- if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- create user
+-- if user doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant privileges to user
+-- to user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- grant perfomance select
+-- to usser
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
