456145f8544a5714dea52e419131303b436b49aa
kurves <kavssimply@gmail.com>
2024-04-17 22:31:58 -0700

    fix importation error

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,8 @@ from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), unique=True, nullable=False, primary_key=True)
