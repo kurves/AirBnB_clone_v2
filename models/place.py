d269dc51be39cfd260dacc22a1f70c1ca9c87598
kurves <kavssimply@gmail.com>
2024-04-17 22:48:25 -0700

    DBStorage - Place

--- models/place.py
+++ models/place.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float
-
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
+from sqlalchemy.ext.declarative import declarative_base
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -18,3 +21,4 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+    amenity_ids = []
