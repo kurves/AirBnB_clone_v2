f79ecaf3edbbec494eab11088340396a17a78f33
kurves <kavssimply@gmail.com>
2024-05-05 02:49:47 -0700

    script to configure

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,9 +1,7 @@
-# Install Nginx if not already installed
 package { 'nginx':
   ensure => installed,
 }
 
-# Create necessary directories if they don't exist
 file { '/data':
   ensure => directory,
   owner  => 'ubuntu',
@@ -17,7 +15,6 @@ file { ['/data/web_static', '/data/web_static/releases', '/data/web_static/share
   require => File['/data'],
 }
 
-# Create a fake HTML file
 file { '/data/web_static/releases/test/index.html':
   ensure  => present,
   content => "Fake content\n",
@@ -26,7 +23,6 @@ file { '/data/web_static/releases/test/index.html':
   require => File['/data/web_static/releases/test'],
 }
 
-# Create a symbolic link /data/web_static/current
 file { '/data/web_static/current':
   ensure  => link,
   target  => '/data/web_static/releases/test',
@@ -36,14 +32,12 @@ file { '/data/web_static/current':
   require => File['/data/web_static/releases/test/index.html'],
 }
 
-# Give ownership of /data/ folder to the ubuntu user and group recursively
 file { '/data':
   owner   => 'ubuntu',
   group   => 'ubuntu',
   recurse => true,
 }
 
-# Update Nginx configuration to serve the content of /data/web_static/current to hbnb_static
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
   content => template('nginx/default.erb'),
@@ -51,7 +45,6 @@ file { '/etc/nginx/sites-available/default':
   notify  => Service['nginx'],
 }
 
-# Restart Nginx
 service { 'nginx':
   ensure    => running,
   enable    => true,
