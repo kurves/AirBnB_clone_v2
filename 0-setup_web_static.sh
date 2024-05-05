2fd58008aa753fea58c73a6150a38399e8f74d78
kurves <kavssimply@gmail.com>
2024-05-05 01:01:21 -0700

    script to configure server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,20 +6,15 @@ if ! dpkg -s nginx &> /dev/null; then
     sudo apt-get install -y nginx
 fi
 
-# Create necessary directories if they don't exist
 sudo mkdir -p /data/web_static/{releases/test,shared}
 
-# Create fake HTML file
 echo "Fake content" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Create symbolic link
 sudo rm -rf /data/web_static/current
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of /data/ folder to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
 nginx_config="
 server {
     listen 80;
@@ -36,5 +31,4 @@ server {
 "
 sudo bash -c "echo '$nginx_config' > /etc/nginx/sites-available/default"
 
-# Restart Nginx
 sudo service nginx restart
