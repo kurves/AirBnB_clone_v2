5df4d93c0d6f6e9707a06d0fddad7a9e4154b415
kurves <kavssimply@gmail.com>
2024-05-05 01:29:07 -0700

    fab sxcript to configure server

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
+""""sscript to configure server"""
 from fabric import task
 from datetime import datetime
 import os
 
 @task
 def do_pack(c):
-    # Create the versions directory if it doesn't exist
+    """script to configure server"""
     c.run('mkdir -p versions')
 
-    # Get the current date and time
     now = datetime.now()
     timestamp = now.strftime("%Y%m%d%H%M%S")
 
-    # Name of the archive
     archive_name = f"web_static_{timestamp}.tgz"
 
-    # Path to the archive
     archive_path = os.path.join("versions", archive_name)
 
-    # Compress web_static folder into a .tgz archive
     result = c.local(f'tar -czvf {archive_path} web_static')
 
-    # Check if the archive has been correctly generated
     if result.failed:
         return None
     else:
