e6c6b22a83aed7bb60236053350b64261f6f8a8e
kurves <kavssimply@gmail.com>
2024-05-05 00:40:31 -0700

    bash script to configure server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,51 +1,40 @@
 #!/usr/bin/env bash
 # script to configure server for deployment
-from fabric import task
 
-@task
-def setup_web_static(c):
-    # Install Nginx if not already installed
-    c.sudo('apt-get update')
-    c.sudo('apt-get install -y nginx')
+if ! dpkg -s nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
 
-    # Create necessary directories if they don't exist
-    directories = [
-        '/data/',
-        '/data/web_static/',
-        '/data/web_static/releases/',
-        '/data/web_static/shared/',
-        '/data/web_static/releases/test/'
-    ]
-    for directory in directories:
-        c.sudo(f'mkdir -p {directory}')
+# Create necessary directories if they don't exist
+sudo mkdir -p /data/web_static/{releases/test,shared}
 
-    # Create fake HTML file
-    c.sudo('echo "Fake content" | sudo tee /data/web_static/releases/test/index.html')
+# Create fake HTML file
+echo "Fake content" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-    # Create symbolic link
-    c.sudo('rm -rf /data/web_static/current')
-    c.sudo('ln -s /data/web_static/releases/test/ /data/web_static/current')
+# Create symbolic link
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
-    # Give ownership of /data/ folder to ubuntu user and group
-    c.sudo('chown -R ubuntu:ubuntu /data/')
+# Give ownership of /data/ folder to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
 
-    # Update Nginx configuration
-    nginx_config = """
-    server {
-        listen 80;
-        server_name _;
+# Update Nginx configuration
+nginx_config="
+server {
+    listen 80;
+    server_name _;
 
-        location /hbnb_static {
-            alias /data/web_static/current/;
-        }
-
-        location / {
-            return 404;
-        }
+    location /hbnb_static {
+        alias /data/web_static/current/;
     }
-    """
-    with c.sudo('bash -c "echo \'{}\' > /etc/nginx/sites-available/default"'.format(nginx_config)):
-        pass
 
-    # Restart Nginx
-    c.sudo('service nginx restart')
+    location / {
+        return 404;
+    }
+}
+"
+sudo bash -c "echo '$nginx_config' > /etc/nginx/sites-available/default"
+
+# Restart Nginx
+sudo service nginx restart
