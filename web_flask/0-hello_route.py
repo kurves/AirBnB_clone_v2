d238454c2518e4fef6967cafad0acf904a83ceb3
kurves <kavssimply@gmail.com>
2024-05-17 12:49:15 +0000

    script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,6 +6,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", strict_slashes=False))
 def hello_world():
-    return "<p>HGHhHHello HBNB!</p>"
+	"""Return hbnb"""
+	return "<p>Hello HBNB!</p>"
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000)
