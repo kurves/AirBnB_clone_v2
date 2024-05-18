6c34b449e051ecd45f04e3775d14e8873fe6aa2a
kurves <kavssimply@gmail.com>
2024-05-18 07:50:41 +0000

    pycodestyle; fix styling and formattingF

--- models/state.py
+++ models/state.py
@@ -14,13 +14,12 @@ class State(BaseModel):
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete", backref="state")
     if getenv('HBNB_TYPE_STORAGE') != 'db':
-    
-    @property
-    def cities(self):
-        """Getter attribute that returns the list of City instances"""
-        city_list = []
-        all_cities = models.storage.all(City)
-        for city in all_cities.values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+        @property
+        def cities(self):
+            """Getter attribute that returns the list of City instances"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
