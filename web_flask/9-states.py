b36aebb5ddd2eb6cad018d9bef7d8cb022b725bf
kurves <kavssimply@gmail.com>
2024-05-21 05:59:01 +0000

    script to start flask appilcation

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,25 +9,18 @@ from models.state import State
 app = Flask(__name__)
 
 @app.route('/states', strict_slashes=False)
-def states_list():
+def states():
     """Display a HTML of all State objects"""
-    states = storage.all(State).values()
+    states = storage.all("State").values()
     return render_template('9-states.html', states=sorted(states, key=lambda x: x.name))
 
 @app.route('/states/<id>', strict_slashes=False)
 def state_cities(id):
     """Display a HTML  of cities of a specific state"""
-    states = storage.all(State).values()
-    state = None
-    for st in states:
-        if st.id == id:
-            state = st
-            break
-    if state:
-        return render_template('9-states.html', state=state)
-    else:
-        return render_template('not_found.html')
-
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 @app.teardown_appcontext
 def teardown_db(exception):
     """Close the current SQLAlchemy Session"""
